7b176cd3354023723b23fe17c37ac6d6744646d1
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-06 22:09:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  attr_accessor :l, :r, :d
+  def initialize left, right, down
+    @l, @r, @d = left, right, down
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    valided_triangle = [(l>0),(r>0),(d>0),(l + r > d), (l + d > r), (d + r > l)]
+    raise TriangleError if valided_triangle.include?(false)
+  end
+    
 end
