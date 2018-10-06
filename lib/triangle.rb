0db00fc3574935be12dc95283a843e26d9e31a35
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-06 22:11:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,13 @@ class Triangle
   def kind
     valided_triangle = [(l>0),(r>0),(d>0),(l + r > d), (l + d > r), (d + r > l)]
     raise TriangleError if valided_triangle.include?(false)
+    if l == r && r == d
+      :equilateral
+    elsif l == r || l == d || r == d 
+      :isosceles
+    else
+      :scalene
+    end
   end
 
 end
